c7bf5425ec4847a9f508bb0f96195ae700c2f3bc
Larry Jackson <45368772+seaguli@users.noreply.github.com>
2020-06-17 03:23:57 -0400

    Update random.js

--- src/cmd/misc/random/random.js
+++ src/cmd/misc/random/random.js
@@ -8,12 +8,17 @@ exports.loadAsSubcommands = true;
 exports.commands = [
     'chirp',
     'woof',
-    'meow'
+    'meow',
+    'fox',
+    'koala',
+    'panda',
+    'goose',
+    'lizard',
 ];
 
 exports.main = {
-    desc: 'Spews out a random image of a birb, a dog or a cat',
-    usage: '<woof | meow | chirp>'
+    desc: 'Spews out a random image',
+    usage: '<woof | meow | chirp | fox | koala | panda | goose | lizard>'
 };
 
 exports.chirp = {
@@ -23,9 +28,65 @@ exports.chirp = {
 
         let res = await got('https://some-random-api.ml/img/birb');
          res = JSON.parse(res.body).link;
+        
+        if(ctx.author.bot) return;
 
         await ctx.createMessage({embed: {
             title: 'What a cute little birby!',
+            image: {url: res},
+            footer: {text: 'Powered by Some Random API'}
+        }});
+    }
+};
+
+exports.fox = {
+    desc: 'Random fox images.',
+    async main(bot, ctx) {
+        await ctx.channel.sendTyping();
+
+        let res = await got('https://some-random-api.ml/img/fox');
+         res = JSON.parse(res.body).link;
+        
+        if(ctx.author.bot) return;
+
+        await ctx.createMessage({embed: {
+            title: 'What does the fox say?',
+            image: {url: res},
+            footer: {text: 'Powered by Some Random API'}
+        }});
+    }
+};
+
+exports.koala = {
+    desc: 'Random koala images.',
+    async main(bot, ctx) {
+        await ctx.channel.sendTyping();
+
+        let res = await got('https://some-random-api.ml/img/koala');
+         res = JSON.parse(res.body).link;
+        
+        if(ctx.author.bot) return;
+
+        await ctx.createMessage({embed: {
+            title: 'Two thumbs up!',
+            image: {url: res},
+            footer: {text: 'Powered by Some Random API'}
+        }});
+    }
+};
+
+exports.panda = {
+    desc: 'Random panda images.',
+    async main(bot, ctx) {
+        await ctx.channel.sendTyping();
+
+        let res = await got('https://some-random-api.ml/img/panda');
+         res = JSON.parse(res.body).link;
+        
+        if(ctx.author.bot) return;
+
+        await ctx.createMessage({embed: {
+            title: 'Have a panda!',
             image: {url: res},
             footer: {text: 'Powered by Some Random API'}
         }});
@@ -37,13 +98,15 @@ exports.meow = {
     async main(bot, ctx) {
         await ctx.channel.sendTyping();
 
-         let res = await got('http://aws.random.cat/meow');
+        let res = await got('http://aws.random.cat/meow');
          res = JSON.parse(res.body).file;
+        
+        if(ctx.author.bot) return;
 
         await ctx.createMessage({embed: {
             title: 'Nyaaa~',
             image: {url: res},
-            footer: {text: 'Powered by random.cat'}
+            footer: {text: 'Powered by Cat as a service'}
         }});
     }
 };
@@ -54,11 +117,49 @@ exports.woof = {
         await ctx.channel.sendTyping();
 
         let res = await got('http://random.dog/woof');
+        
+        if(ctx.author.bot) return;
 
         await ctx.createMessage({embed: {
             title: 'Have a random doggo!',
             image: {url: `http://random.dog/${res.body}`},
             footer: {text: 'Powered by random.dog'}
+        }});
+    }
+};
+
+exports.goose = {
+    desc: 'Random goose images.',
+    async main(bot, ctx) {
+        await ctx.channel.sendTyping();
+
+        let res = await got('https://nekos.life/api/v2/img/goose');
+         res = JSON.parse(res.body).url;
+        
+        if(ctx.author.bot) return;
+
+        await ctx.createMessage({embed: {
+            title: 'Honk Honk!',
+            image: {url: res},
+            footer: {text: 'Powered by nekos.life?!'}
+        }});
+    }
+};
+
+exports.lizard = {
+    desc: 'Random lizard images.',
+    async main(bot, ctx) {
+        await ctx.channel.sendTyping();
+
+        let res = await got('https://nekos.life/api/v2/img/lizard');
+         res = JSON.parse(res.body).url;
+        
+        if(ctx.author.bot) return;
+
+        await ctx.createMessage({embed: {
+            title: 'Here is a lizard!',
+            image: {url: res},
+            footer: {text: 'Powered by nekos.life?!'}
         }});
     }
 };
